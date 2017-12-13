81ebe6e0f23fbf0a4487329cd305629ca5f96220
shagia <mrkraiola@gmail.com>
2017-12-12 17:35:39 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*function showMenu() {
+  $("#MenuMaster").fadeToggle(200);
+  $("#projectDetail").fadeToggle(200);
+};*/
+
+$("#slideShow").click(function() {
+	console.log("clikd");
+	$(".page").fadeToggle(200);
+	
+});

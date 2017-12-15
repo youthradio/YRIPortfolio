25b0b5f51154247a852ce5115761197772b765b6
shagia <mrkraiola@gmail.com>
2017-12-14 17:58:21 -0800

    content update

--- js/main.js
+++ js/main.js
@@ -3,8 +3,41 @@
   $("#projectDetail").fadeToggle(200);
 };*/
 
+const tilt = $('.js-tilt').tilt();
+
+$('.tilt').tilt({
+    scale: 0.96
+})
+
+$(function() {
+    $("#slides").slidesjs({
+        width: 700,
+        height: 700,
+
+        play: {
+            active: true,
+            effect: "slide",
+            interval: 4500,
+            auto: true,
+        },
+        effect: {
+            slide: {
+                speed: 500,
+            },
+        },
+    });
+});
+
+
 $("#slideShow").click(function() {
-	console.log("clikd");
-	$(".page").fadeToggle(200);
-	
+    console.log("Slideshow activated");
+    $(".page").fadeToggle(200);
+    $("#slideClose").fadeToggle(200);
+
+});
+
+$("#slideClose").click(function() {
+    console.log("Slideshow deactivated");
+    $(".page").fadeToggle(200);
+    $("#slideClose").fadeToggle(200);
 });

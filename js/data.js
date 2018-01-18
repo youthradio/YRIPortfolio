83a6f01e281cecf343c7de84dfe9d5565358fa39
shagia <mrkraiola@gmail.com>
2018-01-17 17:58:48 -0800

    markup-js update

--- js/data.js
+++ js/data.js
@@ -6,6 +6,8 @@ var projectTiles = {
 	P5:"Little Rock 9",
 	P6:"Run 4 Prez",
 	P7:"West Side Stories",
+	P8:"No Excuses",
+	P9:"Little Rock 9",
 }
 
 var projectDescriptions = {
@@ -16,16 +18,75 @@ var projectDescriptions = {
 	P5:"Duis quam nisl, convallis eget diam non, dictum tempor dui. Donec tempor, lorem et molestie mollis, leo libero pellentesque ipsum, at egestas libero magna sed sem. Etiam enim ligula, consectetur vel velit quis, hendrerit cursus metus. Sed vulputate hendrerit ligula, at tristique quam mollis ac. Nam a molestie mi. Aenean ut nisi pharetra, hendrerit felis ac, vulputate risus. Praesent erat neque, mattis vitae vestibulum ut, varius vitae libero. Morbi a vehicula orci, in gravida arcu. Cras turpis lectus, vehicula sit amet aliquam vitae, luctus vitae elit. Proin dolor nibh, mattis ac nibh vel, viverra tempor orci. Nunc lobortis ipsum quis arcu egestas, ac lobortis ex faucibus.",
 	P6:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec tortor tristique, dictum mauris et, tincidunt massa. Vivamus at faucibus dolor, eu bibendum mi. Nam et nisi felis. Vestibulum vulputate ipsum dolor, et ultrices erat consectetur ac. Nulla nec leo augue. Curabitur id feugiat velit. Phasellus imperdiet tellus eget nisl condimentum, sed blandit nulla convallis. Integer tincidunt eget nulla eget congue. Proin sed ornare sapien. Praesent tempor convallis lacus at placerat.",
 	P7:"Etiam nec dictum leo. Suspendisse bibendum lacus vehicula diam consequat, sit amet rhoncus est rhoncus. Fusce mollis iaculis nulla vel posuere. Nunc sem elit, pretium vel mauris at, consequat vulputate mi. Phasellus sed iaculis diam. Sed et tincidunt justo, sit amet interdum purus. Maecenas pretium eget lorem sed viverra. Praesent consequat arcu in risus vestibulum scelerisque. Proin dignissim et turpis at scelerisque. Vivamus erat justo, tempor id viverra at, tempor dignissim tellus. Vestibulum non nulla orci. Suspendisse sed lectus odio. Fusce tortor mauris, bibendum non purus et, mattis interdum ipsum. Mauris non varius tellus, sed porta felis. Vestibulum vitae maximus nisi, et gravida leo.",
+	P8:"Sed fermentum, sapien sit amet dignissim efficitur, ligula tortor facilisis ex, at ornare nunc metus non velit. Duis efficitur lorem id odio dapibus commodo. Mauris risus nisl, vulputate id est a, luctus imperdiet augue. Proin purus arcu, faucibus a imperdiet in, tristique a tellus. Cras ut ipsum ipsum. Suspendisse potenti. Nunc condimentum felis at metus gravida, vel aliquam orci lacinia. Duis nec nunc sed felis varius molestie. Maecenas viverra magna ut felis porta elementum. Quisque lacinia lectus ac urna volutpat, sed consectetur purus tristique. Donec id urna vestibulum, ultricies magna id, porta quam. Curabitur consequat erat vel purus porta congue.",
+	P9:"Etiam nec dictum leo. Suspendisse bibendum lacus vehicula diam consequat, sit amet rhoncus est rhoncus. Fusce mollis iaculis nulla vel posuere. Nunc sem elit, pretium vel mauris at, consequat vulputate mi. Phasellus sed iaculis diam. Sed et tincidunt justo, sit amet interdum purus. Maecenas pretium eget lorem sed viverra. Praesent consequat arcu in risus vestibulum scelerisque. Proin dignissim et turpis at scelerisque. Vivamus erat justo, tempor id viverra at, tempor dignissim tellus. Vestibulum non nulla orci. Suspendisse sed lectus odio. Fusce tortor mauris, bibendum non purus et, mattis interdum ipsum. Mauris non varius tellus, sed porta felis. Vestibulum vitae maximus nisi, et gravida leo.",
+
 }
 
-var projectCredits = {
-	P1:"No Excuses",
-	P2:"GENXYZ",
-	P3:"Mood Ring",
-	P4:"Don't Get Faked",
-	P5:"Little Rock 9",
-	P6:"Run 4 Prez",
-	P7:"West Side Stories",
+//There's a better way of doing this, this is plain lazy
+
+var p1Credits = {
+	Name1:"Andy Kirby",
+	Name2:"Melanie	Barnett",
+	Name3:"Bill	Collier",
+	Name4:"Jo Mccormick",
+	Name5:"Jesus Boyd",
+}
+var p2Credits = {
+	Name1:"Roxy	Walden",
+	Name2:"Ellis Ma",
+	Name3:"Bobbi Coles",
+	Name4:"Jame Ainsworth",
+	Name5:"Juliane	Bui",
+}
+var p3Credits = {
+	Name1:"Rudy	Guenther",
+	Name2:"Sean	Griffin",
+	Name3:"Lori	Allen",
+	Name4:"John	Perez",
+	Name5:"Mary	Thompson",
+}
+var p4Credits = {
+	Name1:"Roxy	Walden",
+	Name2:"Ellis Ma",
+	Name3:"Bobbi Coles",
+	Name4:"Jame Ainsworth",
+	Name5:"Juliane	Bui",
+}
+var p5Credits = {
+	Name1:"Andy Kirby",
+	Name2:"Melanie	Barnett",
+	Name3:"Bill	Collier",
+	Name4:"Jo Mccormick",
+	Name5:"Jesus Boyd",
+}
+var p6Credits = {
+	Name1:"John	Perez",
+	Name2:"Sean	Phillips",
+	Name3:"Deborah	Lopez",
+	Name4:"Antonietta	Fitzsimmons",
+	Name5:"Jesus Boyd",
+}
+var p7Credits = {
+	Name1:"Roxy	Walden",
+	Name2:"Ellis Ma",
+	Name3:"Bobbi Coles",
+	Name4:"Jame Ainsworth",
+	Name5:"Juliane	Bui",
+}
+var p8Credits = {
+	Name1:"Andy Kirby",
+	Name2:"Melanie	Barnett",
+	Name3:"Bill	Collier",
+	Name4:"Jo Mccormick",
+	Name5:"Jesus Boyd",
+}
+var p9Credits = {
+	Name1:"Rudy	Guenther",
+	Name2:"Sean	Griffin",
+	Name3:"Lori	Allen",
+	Name4:"John	Perez",
+	Name5:"Mary	Thompson",
 }
 
 var projectLink = {
--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 
 Object.values(projectTiles);
 Object.values(projectDescriptions);
+Object.values(p1Credits);
 
 const tilt = $('.js-tilt').tilt();
 
@@ -58,6 +59,11 @@ $("#hPic1").click(function() {
     $("#P1TitleC").text(projectTiles.P1);
     $("#P1Title").text(projectTiles.P1);
     $("#P1Desc").text(projectDescriptions.P1);
+    $("#P1Credit1").text(p1Credits.Name1);
+    $("#P1Credit2").text(p1Credits.Name2);
+    $("#P1Credit3").text(p1Credits.Name3);
+    $("#P1Credit4").text(p1Credits.Name4);
+    $("#P1Credit5").text(p1Credits.Name5);
     document.getElementById("P1Link").href = "http://google.com";
 });
 
@@ -69,6 +75,11 @@ $("#hPic2").click(function() {
     $("#P1TitleC").text(projectTiles.P2);
     $("#P1Title").text(projectTiles.P2);
     $("#P1Desc").text(projectDescriptions.P2);
+    $("#P1Credit1").text(p2Credits.Name1);
+    $("#P1Credit2").text(p2Credits.Name2);
+    $("#P1Credit3").text(p2Credits.Name3);
+    $("#P1Credit4").text(p2Credits.Name4);
+    $("#P1Credit5").text(p2Credits.Name5);
 });
 
 $("#hPic3").click(function() {
@@ -79,6 +90,11 @@ $("#hPic3").click(function() {
     $("#P1TitleC").text(projectTiles.P3);
     $("#P1Title").text(projectTiles.P3);
     $("#P1Desc").text(projectDescriptions.P3);
+    $("#P1Credit1").text(p3Credits.Name1);
+    $("#P1Credit2").text(p3Credits.Name2);
+    $("#P1Credit3").text(p3Credits.Name3);
+    $("#P1Credit4").text(p3Credits.Name4);
+    $("#P1Credit5").text(p3Credits.Name5);
 });
 
 $("#hPic4").click(function() {
@@ -89,6 +105,11 @@ $("#hPic4").click(function() {
     $("#P1TitleC").text(projectTiles.P4);
     $("#P1Title").text(projectTiles.P4);
     $("#P1Desc").text(projectDescriptions.P4);
+    $("#P1Credit1").text(p4Credits.Name1);
+    $("#P1Credit2").text(p4Credits.Name2);
+    $("#P1Credit3").text(p4Credits.Name3);
+    $("#P1Credit4").text(p4Credits.Name4);
+    $("#P1Credit5").text(p4Credits.Name5);
 });
 
 $("#hPic5").click(function() {
@@ -99,6 +120,11 @@ $("#hPic5").click(function() {
     $("#P1TitleC").text(projectTiles.P5);
     $("#P1Title").text(projectTiles.P5);
     $("#P1Desc").text(projectDescriptions.P5);
+    $("#P1Credit1").text(p5Credits.Name1);
+    $("#P1Credit2").text(p5Credits.Name2);
+    $("#P1Credit3").text(p5Credits.Name3);
+    $("#P1Credit4").text(p5Credits.Name4);
+    $("#P1Credit5").text(p5Credits.Name5);
 });
 
 $("#hPic6").click(function() {
@@ -109,6 +135,11 @@ $("#hPic6").click(function() {
     $("#P1TitleC").text(projectTiles.P6);
     $("#P1Title").text(projectTiles.P6);
     $("#P1Desc").text(projectDescriptions.P6);
+    $("#P1Credit1").text(p6Credits.Name1);
+    $("#P1Credit2").text(p6Credits.Name2);
+    $("#P1Credit3").text(p6Credits.Name3);
+    $("#P1Credit4").text(p6Credits.Name4);
+    $("#P1Credit5").text(p6Credits.Name5);
 });
 
 $("#hPic7").click(function() {
@@ -119,11 +150,182 @@ $("#hPic7").click(function() {
     $("#P1TitleC").text(projectTiles.P7);
     $("#P1Title").text(projectTiles.P7);
     $("#P1Desc").text(projectDescriptions.P7);
+    $("#P1Credit1").text(p7Credits.Name1);
+    $("#P1Credit2").text(p7Credits.Name2);
+    $("#P1Credit3").text(p7Credits.Name3);
+    $("#P1Credit4").text(p7Credits.Name4);
+    $("#P1Credit5").text(p7Credits.Name5);
+});
+
+$("#hPic8").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P8);
+    $("#P1Title").text(projectTiles.P8);
+    $("#P1Desc").text(projectDescriptions.P8);
+    $("#P1Credit1").text(p8Credits.Name1);
+    $("#P1Credit2").text(p8Credits.Name2);
+    $("#P1Credit3").text(p8Credits.Name3);
+    $("#P1Credit4").text(p8Credits.Name4);
+    $("#P1Credit5").text(p8Credits.Name5);
+});
+
+$("#hPic9").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P9);
+    $("#P1Title").text(projectTiles.P9);
+    $("#P1Desc").text(projectDescriptions.P9);
+    $("#P1Credit1").text(p9Credits.Name1);
+    $("#P1Credit2").text(p9Credits.Name2);
+    $("#P1Credit3").text(p9Credits.Name3);
+    $("#P1Credit4").text(p9Credits.Name4);
+    $("#P1Credit5").text(p9Credits.Name5);
+});
+
+$("#navHPic1").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P1);
+    $("#P1Title").text(projectTiles.P1);
+    $("#P1Desc").text(projectDescriptions.P1);
+    document.getElementById("P1Link").href = "http://google.com";
+    $("#P1Credit1").text(p1Credits.Name1);
+    $("#P1Credit2").text(p1Credits.Name2);
+    $("#P1Credit3").text(p1Credits.Name3);
+    $("#P1Credit4").text(p1Credits.Name4);
+    $("#P1Credit5").text(p1Credits.Name5);
+});
+
+$("#navHPic2").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P2);
+    $("#P1Title").text(projectTiles.P2);
+    $("#P1Desc").text(projectDescriptions.P2);
+    $("#P1Credit1").text(p2Credits.Name1);
+    $("#P1Credit2").text(p2Credits.Name2);
+    $("#P1Credit3").text(p2Credits.Name3);
+    $("#P1Credit4").text(p2Credits.Name4);
+    $("#P1Credit5").text(p2Credits.Name5);
+});
+
+$("#navHPic3").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P3);
+    $("#P1Title").text(projectTiles.P3);
+    $("#P1Desc").text(projectDescriptions.P3);
+    $("#P1Credit1").text(p3Credits.Name1);
+    $("#P1Credit2").text(p3Credits.Name2);
+    $("#P1Credit3").text(p3Credits.Name3);
+    $("#P1Credit4").text(p3Credits.Name4);
+    $("#P1Credit5").text(p3Credits.Name5);
+});
+
+$("#navHPic4").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P4);
+    $("#P1Title").text(projectTiles.P4);
+    $("#P1Desc").text(projectDescriptions.P4);
+    $("#P1Credit1").text(p4Credits.Name1);
+    $("#P1Credit2").text(p4Credits.Name2);
+    $("#P1Credit3").text(p4Credits.Name3);
+    $("#P1Credit4").text(p4Credits.Name4);
+    $("#P1Credit5").text(p4Credits.Name5);
+});
+
+$("#navHPic5").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P5);
+    $("#P1Title").text(projectTiles.P5);
+    $("#P1Desc").text(projectDescriptions.P5);
+    $("#P1Credit1").text(p5Credits.Name1);
+    $("#P1Credit2").text(p5Credits.Name2);
+    $("#P1Credit3").text(p5Credits.Name3);
+    $("#P1Credit4").text(p5Credits.Name4);
+    $("#P1Credit5").text(p5Credits.Name5);
+});
+
+$("#navHPic6").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P6);
+    $("#P1Title").text(projectTiles.P6);
+    $("#P1Desc").text(projectDescriptions.P6);
+    $("#P1Credit1").text(p6Credits.Name1);
+    $("#P1Credit2").text(p6Credits.Name2);
+    $("#P1Credit3").text(p6Credits.Name3);
+    $("#P1Credit4").text(p6Credits.Name4);
+    $("#P1Credit5").text(p6Credits.Name5);
+});
+
+$("#navHPic7").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P7);
+    $("#P1Title").text(projectTiles.P7);
+    $("#P1Desc").text(projectDescriptions.P7);
+    $("#P1Credit1").text(p7Credits.Name1);
+    $("#P1Credit2").text(p7Credits.Name2);
+    $("#P1Credit3").text(p7Credits.Name3);
+    $("#P1Credit4").text(p7Credits.Name4);
+    $("#P1Credit5").text(p7Credits.Name5);
+});
+
+$("#navHPic8").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P8);
+    $("#P1Title").text(projectTiles.P8);
+    $("#P1Desc").text(projectDescriptions.P8);
+    $("#P1Credit1").text(p8Credits.Name1);
+    $("#P1Credit2").text(p8Credits.Name2);
+    $("#P1Credit3").text(p8Credits.Name3);
+    $("#P1Credit4").text(p8Credits.Name4);
+    $("#P1Credit5").text(p8Credits.Name5);
+});
+
+$("#navHPic9").click(function() {
+    $("#P1Accordian").collapse('hide');
+    $('html, body').animate({
+        scrollTop: $("#P1").offset().top
+    }, 500);
+    $("#P1TitleC").text(projectTiles.P9);
+    $("#P1Title").text(projectTiles.P9);
+    $("#P1Desc").text(projectDescriptions.P9);
+    $("#P1Credit1").text(p9Credits.Name1);
+    $("#P1Credit2").text(p9Credits.Name2);
+    $("#P1Credit3").text(p9Credits.Name3);
+    $("#P1Credit4").text(p9Credits.Name4);
+    $("#P1Credit5").text(p9Credits.Name5);
 });
 
 
 
-$(window).scroll(function() {
+/* $(window).scroll(function() {
     var height = $(window).scrollTop();
 
     if(height  > 400) {
@@ -133,7 +335,7 @@ $(window).scroll(function() {
     if (height  < 400) {
         $("#navContent").fadeOut(200);
     }
-});
+}); */
 
 $("#ugh").click(function() {
     $("#navContent").fadeToggle(200);
